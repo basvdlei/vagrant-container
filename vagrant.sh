6a01a69a4be8a140b1675b46206a6995c30c1b15
Bas van der Lei <basvdlei@users.noreply.github.com>
2024-10-25 14:17:15 +0200

    Add run script

---
+++ vagrant.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Add the following flag to enable debug logging.
+# --env "VAGRANT_LOG=debug" \
+exec podrunner.sh --utf8 --homedir --libvirtd --ssh-agent -- \
+	--rm -it \
+	--volume /var/lib:/var/lib:ro \
+	--env "CONTAINER_TYPE=vagrant" \
+        --env "LIBVIRT_DEFAULT_URI=${LIBVIRT_DEFAULT_URI}" \
+	--env "EDITOR=vim" \
+	--net host \
+	--privileged \
+	--entrypoint /usr/bin/vagrant \
+	localhost/vagrant-container:40 "$@"
